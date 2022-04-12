976bda36a06d63e3c878c0fc38c2f197f3940069
matt rice <ratmice@gmail.com>
2022-04-11 22:57:19 -0700

    formatting

--- build.rs
+++ build.rs
@@ -4,9 +4,11 @@ use lrlex::CTLexerBuilder;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     CTLexerBuilder::new()
         .lrpar_config(|ctp| {
-            ctp.yacckind(YaccKind::Original(cfgrammar::yacc::YaccOriginalActionKind::NoAction))
-                .grammar_in_src_dir("crimson.y")
-                .unwrap()
+            ctp.yacckind(YaccKind::Original(
+                cfgrammar::yacc::YaccOriginalActionKind::NoAction,
+            ))
+            .grammar_in_src_dir("crimson.y")
+            .unwrap()
         })
         .lexer_in_src_dir("crimson.l")?
         .build()?;
